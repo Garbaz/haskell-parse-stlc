7eaea84e5035d7b2d72bbdb6c552174340038ea9
garbaz <garbaz@t-online.de>
2022-07-08 11:23:03 +0200

    renamed project to stlc-mod-isom

--- src/TypeCheck.hs
+++ src/TypeCheck.hs
@@ -1,28 +1,13 @@
 module TypeCheck
   ( typeInfer,
     typeCheck,
-    emptyContext,
   )
 where
 
 import Data.Functor ((<&>))
-import qualified Data.Map.Strict as Map
-import Data.Maybe (fromMaybe)
 import LambdaTerm
 import TypeTerm
-
-type TypingContext = Map.Map String [TypeExpr]
-
-emptyContext = Map.empty
-
-lookupVar :: TypingContext -> String -> Maybe TypeExpr
-lookupVar g v = head <$> Map.lookup v g
-
-pushVar :: TypingContext -> String -> TypeExpr -> TypingContext
-pushVar g v t = Map.insert v (t : fromMaybe [] (Map.lookup v g)) g
-
-(?=>) :: Bool -> Maybe a -> Maybe a
-(?=>) p x = if p then x else Nothing
+import TypingContext
 
 typeCheck :: TypingContext -> LambdaExpr -> TypeExpr -> Bool
 typeCheck g (Variable v) tt = case lookupVar g v of
